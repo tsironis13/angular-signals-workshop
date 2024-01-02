190b2c703e0c8693d26ba4b71a77e397d083cd22
alcfeoh <alain@interstate21.com>
2024-01-02 15:36:46 -0800

    Making Jumbotron standalone

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
+  standalone: true,
   styleUrls: ['./jumbotron.component.css']
 })
 export class JumbotronComponent {
